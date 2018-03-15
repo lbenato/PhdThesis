07a70a2c0c1feacf4e064afdfb6abf8862b875e4
Lisa <lisa.benato@pd.infn.it>
2018-03-15 10:52:09 +0100

    v0 of PhD defense talk

---
+++ PhD_defense/2018_03_26_Benato/compila_defense.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+pdflatex 2018_03_26_Benato.tex
+#bibtex Benato_PhD_Thesis
+#bibtex Benato_PhD_Thesis
+#pdflatex 2018_03_26_Benato.tex
+#bibtex Benato_PhD_Thesis
+#pdflatex Benato_PhD_Thesis.tex
+lualatex 2018_03_26_Benato.tex
